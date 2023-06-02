6f488f980161131930c62d6fd3c764979b3da8ca
Samandar2005 <86775489+Samandar2005@users.noreply.github.com>
2023-06-02 09:20:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Anchor havolalari uchun yumshoq aylantirish
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
